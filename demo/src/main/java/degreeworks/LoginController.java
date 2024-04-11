e93bf6538af949717141d4f55d4144355f38ff32
Vito Spatafora <vspatafora27@gmail.com>
2024-04-11 16:52:48 -0400

    Added controller to loginPage

--- demo/src/main/java/degreeworks/LoginController.java
+++ demo/src/main/java/degreeworks/LoginController.java
@@ -1,9 +1,32 @@
 package degreeworks;
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import model.UserList;
+
 public class LoginController {
+
     @FXML
-    private void switchToUserScene() throws IOException {
-        App.setRoot("primary");
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private void initialize() {
+    }
+
+    @FXML
+    private void handleLoginButtonAction() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        if(UserList.getInstance().isValidUser(username, password)){
+            //Log the user in
+        }
     }
 }
+
