caa5f006926e01a1e516c335c78e808dde088884
ayushmp10 <ayushparambath@gmail.com>
2024-04-17 17:18:23 -0400

    Added new Advisor and Course files

---
+++ demo/src/main/java/model/Advisor.java
@@ -0,0 +1,94 @@
+package model;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Advisor extends User{
+    private ArrayList<Student> students;
+    private UserList userList = UserList.getInstance();
+    private ArrayList<Student> allStudents = userList.getStudents();
+    private Student currStudent;
+    private String building;
+    private String roomNumber;
+    public Advisor(UUID id, String username, String password, String firstName, String lastName,
+                    ArrayList<Student> students, String phoneNumber, String building, String roomNumber) {
+        super(id, username, password, firstName, lastName, phoneNumber);// already adds the user to the userlist
+        this.students = students;
+        this.building = building;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBuilding() {
+        return this.building;
+    }
+    public String getRoomNumber() {
+        return this.roomNumber;
+    }
+    public UUID getUUID() {
+        return super.getID();
+    }
+
+    public void populateStudents() {
+        for (Student student : allStudents) {
+            if (student.getAdvisor().equals(this.getID())) {
+                students.add(student);
+            }
+        }
+        if (allStudents.size() > 0) {
+            currStudent = allStudents.get(0);
+        } else {
+            currStudent = null;
+        }
+    }
+
+    public Student getCurrentStudent() {
+        return this.currStudent;
+    }
+    public void setCurrentStudent(Student student) {
+        if (allStudents.contains(student) && student.getAdvisor().equals(this.getID())) {
+            currStudent = student;
+        } else if (!allStudents.contains(student)) {
+            System.out.println("This student does not exist in our system");
+        } else if (!student.getAdvisor().equals(this.getID())) {
+            System.out.println("this is not your student");
+        }
+    }
+    
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+    public void highlightStudentCourses(Student student) {
+    }
+    public ArrayList<Course> getStudentCurrentCourses(Student student) {
+        return new ArrayList<>();
+    }
+    public void addStudent(Student student) {
+        if (allStudents.contains(student))
+            students.add(student);
+    }
+    public void removeStudent(Student student) {
+        if (students.contains(student))
+            students.remove(student);
+    }
+    public Student getStudent(Student studentF) {
+        for (Student student : students) {
+            if (studentF.getID().equals(student.getID())) {
+                return student;
+            }
+        }
+        return null;
+    }
+    public Student getStudent(UUID studentFUUID) {
+        for (Student student : students) {
+            if (student.getID().equals(studentFUUID)) {
+                return student;
+            }
+        } 
+        return null;
+    }
+
+    public String getLocation() {
+        return "Advisor's Office: " + this.building + " " + this.roomNumber;
+    }
+}
+
+
