5fce4f0eb17a63dffb84fcc73bb3bb851efe4b01
ayushmp10 <ayushparambath@gmail.com>
2024-04-16 21:41:53 -0400

    pulling

--- demo/src/main/java/degreeworks/student_changeMajorController.java
+++ demo/src/main/java/degreeworks/student_changeMajorController.java
@@ -1,5 +1,58 @@
 package degreeworks;
 
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
 public class student_changeMajorController {
     
+    @FXML
+    void availableCoursesClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_availableCourses");
+
+    }
+
+    @FXML
+    void changeMajorClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_changeMajor");
+
+    }
+
+    @FXML
+    void commentsClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_comments");
+
+    }
+
+    @FXML
+    void completedCoursesClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_completedCourses");
+
+    }
+
+    @FXML
+    void homeClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_home");
+
+    }
+
+    @FXML
+    void majorMapClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_majorMap");
+
+    }
+
+    @FXML
+    void onLogoutClicked(MouseEvent event) throws IOException{
+        App.setRoot("home");
+
+    }
+
+    @FXML
+    void planGeneratorClicked(MouseEvent event) throws IOException{
+        App.setRoot("student_planGenreator");
+
+    }
 }
