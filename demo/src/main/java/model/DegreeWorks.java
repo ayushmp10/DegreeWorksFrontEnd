0ace1730064e6b5dbf8b9d7268f7086d88490689
ayushmp10 <ayushparambath@gmail.com>
2024-04-19 11:34:03 -0400

    Completed sign up for advisor

--- demo/src/main/java/model/DegreeWorks.java
+++ demo/src/main/java/model/DegreeWorks.java
@@ -20,8 +20,6 @@ public class DegreeWorks {
         return degreeWorks;
     }
 
-    // overload createAccount for students, guardians, and advisors
-    // each one should add to the userlist
     public boolean createAccount(String firstName, String lastName, String phoneNumber, String VIPid, String userName, String password, String profileType) {
         if (profileType.equalsIgnoreCase("student")) {
             // create a student and add it to the user list
@@ -33,6 +31,9 @@ public class DegreeWorks {
         else if (profileType.equalsIgnoreCase("advisor")) {
             // create an advisor and add it to the user list
             // substitute default values for other information
+            Advisor tempAdvisor = new Advisor(firstName, lastName, phoneNumber, VIPid, userName, password);
+            userList.addUser(tempAdvisor);
+            userList.setCurrUser(tempAdvisor);
         }
         else if (profileType.equalsIgnoreCase("guardian")) {
             // create a guardian and add it to the user list
