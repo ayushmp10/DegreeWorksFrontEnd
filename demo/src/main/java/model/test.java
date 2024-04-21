3a9ab2c8a4f944ee4c4682e8796bed56ba5518f5
ayushmp10 <ayushparambath@gmail.com>
2024-04-21 18:02:06 -0400

    Fixed json files

--- demo/src/main/java/model/test.java
+++ demo/src/main/java/model/test.java
@@ -7,7 +7,8 @@ import java.util.UUID;
 public class test {
     public static void main(String[] args) {
         UserList userList = UserList.getInstance();
-        Degree tempDegree = new Degree();
+        DegreeList degreeList = DegreeList.getInstance();
+        Degree tempDegree = degreeList.getDegree(UUID.fromString("dfa84645-4278-44a3-8b3b-c6b4fc6257c5"));
         HashMap<Course, String> tempHashMap = new HashMap<Course, String>();
         ArrayList<Semester> tempSemesters = new ArrayList<Semester>();
         ArrayList<Course> courses = new ArrayList<Course>();
@@ -20,10 +21,13 @@ public class test {
                                 UUID.fromString("152004c3-c655-439d-bbc0-eeaa58f57874"), UUID.fromString("6e30c187-5592-4d8a-91e4-e874f34a41cd"),
                                 "Q313514", "robotics", "none", tempHashMap, tempSemester, tempSemesters);
         userList.addUser(tempStudent);
-        
         ArrayList<Student> allStudents = userList.getStudents();
-        ArrayList<Advisor> allAdvisors = DataLoader.getAdvisors();
+        Advisor tempAdvisor = new Advisor(UUID.randomUUID(), "appp", "12312", "Wanda", "Mujica", allStudents,
+                                "000000", "Swearingen", "1A01");
+        userList.addUser(tempAdvisor);
         DataWriter.saveUsers(userList.getUsers());
-        System.out.println(allAdvisors.size());
+        //ArrayList<Advisor> allAdvisors = DataLoader.getAdvisors();
+        
+        //System.out.println(allAdvisors.size());
     }
 }
