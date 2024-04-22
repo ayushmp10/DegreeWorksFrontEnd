0b5163c2065255d99edcbaba2d51762d1ef70886
Aaron Moore <aaronmoore@Aarons-MacBook-Air-10.local>
2024-04-22 00:24:08 -0400

    switching app to load home switched on accident

--- demo/src/main/java/degreeworks/App.java
+++ demo/src/main/java/degreeworks/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("advisor_home"), 1000, 600);
+        scene = new Scene(loadFXML("home"), 1000, 600);
         stage.setScene(scene);
         stage.show();
     }
