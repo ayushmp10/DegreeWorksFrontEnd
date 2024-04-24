2bdba6505f00a84d4532cbb215a2a86ad7dbcdb2
VitoSpatafora <143433342+VitoSpatafora@users.noreply.github.com>
2024-04-24 05:43:29 -0400

    Update student_completedCoursesController.java

--- demo/src/main/java/degreeworks/student_completedCoursesController.java
+++ demo/src/main/java/degreeworks/student_completedCoursesController.java
@@ -8,16 +8,23 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Font;
 import model.*;
 
 public class student_completedCoursesController implements Initializable{
     private Student currStudent;
     private UserList userList;
     @FXML private GridPane completedCoursePane;
+    @FXML private Label x;
+    @FXML private Label y;
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -27,20 +34,35 @@ public class student_completedCoursesController implements Initializable{
         currStudent = (Student) userList.getCurrUser();
         HashMap<Course, String> completedCourses = currStudent.getCompletedCourses();
         displayCompletedCourses(completedCourses);
+        Font font = Font.font("Arial Black", 14);
+        x.setFont(font);
+        y.setFont(font);
     }
 
     private void displayCompletedCourses(HashMap<Course, String> completedCourses) {
+        int rowHeight = 50;
         // set formatting for gridpane
-        completedCoursePane.getRowConstraints().add(new RowConstraints(completedCourses.size()));
-        completedCoursePane.getColumnConstraints().add(new ColumnConstraints(2));
+        completedCoursePane.getRowConstraints().clear();
+        completedCoursePane.getRowConstraints().add(new RowConstraints(50));
         // run throguh hashmap
-        int row = 0;
+        int row = 1;
         for (Map.Entry<Course, String> gradedCourse : completedCourses.entrySet()) {
-            // add each course to a 2 column gridpane
+            completedCoursePane.getRowConstraints().add(new RowConstraints(50));
+            Font font = Font.font("Arial Black",14);
             Label courseName = new Label(gradedCourse.getKey().getFullName());
             Label courseGrade = new Label(gradedCourse.getValue());
+            courseName.setFont(font);
+            courseName.setAlignment(Pos.CENTER);
+            courseGrade.setFont(font);
+            courseGrade.setAlignment(Pos.CENTER);
+            
+
             completedCoursePane.add(courseName, 0, row);
             completedCoursePane.add(courseGrade, 1, row);
+            GridPane.setHalignment(courseGrade, HPos.CENTER);
+            GridPane.setValignment(courseGrade, VPos.CENTER);
+            GridPane.setHalignment(courseName, HPos.CENTER);
+            GridPane.setValignment(courseName, VPos.CENTER);
             row++;
         }
     }
