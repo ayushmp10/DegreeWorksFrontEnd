e632198571d06acd83abfbe396daf6ca1c18c5b8
VitoSpatafora <143433342+VitoSpatafora@users.noreply.github.com>
2024-04-24 05:45:23 -0400

    Update advisor_allStudentsController.java

--- demo/src/main/java/degreeworks/advisor_allStudentsController.java
+++ demo/src/main/java/degreeworks/advisor_allStudentsController.java
@@ -7,9 +7,12 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import model.Student;
 import model.UserList;
 
@@ -23,13 +26,21 @@ public class advisor_allStudentsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         userList = UserList.getInstance();
         ArrayList<Student> allStudents = userList.getStudents();
-        int rowCount = 0;
+        int rowCount = 1;
+        allStudentInfo.getRowConstraints().clear();
         for (Student student : allStudents) {
             Label studentUSCID = new Label(student.getUSCID());
             Label studentName = new Label(student.getFirstName() + " " + student.getLastName());
             Label studentDegree = new Label(student.getDegree().getSubject());
 
             allStudentInfo.addRow(rowCount, studentUSCID, studentName, studentDegree);
+            GridPane.setValignment(studentDegree, VPos.CENTER);
+            GridPane.setValignment(studentUSCID, VPos.CENTER);
+            GridPane.setValignment(studentDegree, VPos.CENTER);
+            GridPane.setHalignment(studentDegree, HPos.CENTER);
+            GridPane.setHalignment(studentUSCID, HPos.CENTER);
+            GridPane.setHalignment(studentName, HPos.CENTER);
+            allStudentInfo.getRowConstraints().add(new RowConstraints(20));
             rowCount++;
         }
     }
